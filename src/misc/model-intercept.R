8dc117ea9aff663238af19fc547e0703daa93280
MonsterZero <monsterzero@mac.mynetworksettings.com>
2025-05-23 21:35:08 -0400

    removed src/misc which contained the old scripts before the 3 study format

--- src/misc/model-intercept.R
+++
@@ -1,9 +0,0 @@
-
-m_intercept <- 
-  brm(bipf_category ~ 1,
-      data = data_scaled,
-      family = cumulative(threshold = "flexible"),
-      cores = 4)
-
-summary(m_intercept)
-get_prior(m_intercept)
